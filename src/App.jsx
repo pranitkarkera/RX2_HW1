b75bfcaf4f76f7c5c5eba3565f611a3ce7b37511
Pranit Karkera <50833762+pranitkarkera@users.noreply.github.com>
2024-12-28 22:56:40 +0530

    Add files via upload
    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import FinanceSummary from "./financeSummary";
+import IncomeExpense from "./IncomeExpenseForm";
+
+function App(){
+    return(
+      <div>
+        <IncomeExpense />
+        <FinanceSummary/>
+      </div>
+    )
+}
+export default App;
